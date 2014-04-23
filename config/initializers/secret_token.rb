3f14785180fc7fdc127fc352199023df60d65419
Stephen Moock <stephen.moock@gmail.com>
2014-04-22 22:13:11 -0700

    added all my files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moockbrew::Application.config.secret_key_base = 'f5d454dcad5d4715ce813d3a9e759e64e418027616c5b8f5e3ac4761887627fa8fcc8beb0824adf92f5208d84544b1cfa88c3c5e4c3f8e32ecbdb5a3e7cfde51'
